4231e6b14c261336322a400b10a459170e818bce
元彦 <yuanyan.cao@alibaba-inc.com>
2015-01-06 18:54:46 +0800

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# jstx
+> Compile templates to a JST file.
+
+## License
+Copyright (c) 2015 Yuanyan Cao. Licensed under the MIT license.
---
+++ test/jstx_test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var assert = require('assert');
+var jst = require('../');
+
+var template = jst('<%=foo%>');
+
+assert.equal(template({foo:'foo'}), 'foo');
